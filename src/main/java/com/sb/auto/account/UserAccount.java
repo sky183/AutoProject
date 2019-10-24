fc44a8af08e9076294171ed454a24c3ec88a74f8
sky183 <sky18333@naver.com>
2019-10-24 17:57:09 +0900

    Initial commit

---
+++ src/main/java/com/sb/auto/account/UserAccount.java
@@ -0,0 +1,20 @@
+package com.sb.auto.account;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Arrays;
+
+public class UserAccount extends User {
+
+    private Account account;
+
+    public UserAccount(Account account) {
+        super(account.getUsername(), account.getPassword(), Arrays.asList(new SimpleGrantedAuthority("ROLE_" + account.getRole())));
+        this.account = account;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+}
---
+++ src/main/java/com/sb/auto/service/SampleService.java
@@ -0,0 +1,28 @@
+package com.sb.auto.service;
+
+import com.sb.auto.common.SecurityLogger;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+    @Secured({"ROLE_USER", "ROLE_ADMIN"})
+    public void dashboard() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        System.out.println("===============");
+        System.out.println(authentication);
+        System.out.println(userDetails.getUsername());
+    }
+
+    @Async
+    public void asyncService() {
+        SecurityLogger.log("Async Service");
+        System.out.println("Async service is called.");
+    }
+}
